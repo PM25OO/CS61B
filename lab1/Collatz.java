5874c24ddc721e367816fa95c674acc4f6dcc165
PM25OO <3210636476@qq.com>
2025-04-07 15:26:28 +0800

    update

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -14,15 +14,15 @@ public class Collatz {
      *         return 3 * n + 1;
      * }
      */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
-    }
+    public static int nextNumber(int n){
+          if (n == 1)
+              return n;
+          if (n % 2 == 0){
+              return n / 2;
+          }
+          else
+              return 3 * n + 1;
+      }
 
     public static void main(String[] args) {
         int n = 5;

9a6fdd47a8ea23e6f615e27324353c49598e4192
PM25OO <3210636476@qq.com>
2025-05-26 16:19:33 +0800

    Add ArrayDeque class

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,12 @@
+package deque;
+
+public class ArrayDeque<T> {
+    private int size;
+    private T item;
+    private T first;
+    private T last;
+
+    public ArrayDeque () {
+        T[] List = (T []) new Object[8];
+    }
+}

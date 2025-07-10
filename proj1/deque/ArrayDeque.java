2c1cec376a05b17a72450b996e9142fb2cc4e32a
PM25OO <3210636476@qq.com>
2025-07-10 21:49:10 +0800

    update methods

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,12 +1,49 @@
 package deque;
 
 public class ArrayDeque<T> {
-    private int size;
+    static double RFACTOR = 0.25;
+    private int size = 0;
+    private int max;
+    private T[] list;
     private T item;
     private T first;
     private T last;
 
     public ArrayDeque () {
-        T[] List = (T []) new Object[8];
+        list = (T []) new Object[8];
+        max = 8;
+        first = list[0];
+        last = list[size];
+    }
+
+    public void addFirst(T item) {
+        indexOverOrNot();
+        System.arraycopy(list, 0, list, 1, size);
+        list[0] = item;
+        size += 1;
+    }
+    
+    public void addLast(T item) {
+        indexOverOrNot();
+        list[size] = item;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    private void indexOverOrNot() {
+        if (size == max) {
+            increaseSize();
+        } else if ((double) size/max < RFACTOR) {
+            lowerSize();
+        }
+    }
+
+    private void lowerSize() {
+    }
+
+    private void increaseSize() {
     }
 }

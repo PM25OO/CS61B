ad1c1b342433d389f7a14d23159b7a4f6db90339
PM25OO <3210636476@qq.com>
2025-07-24 17:21:42 +0800

    finish task2

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,19 +1,14 @@
 package deque;
 
 public class ArrayDeque<T> {
-    static double RFACTOR = 0.25;
+    static double R_FACTOR = 0.25;
     private int size = 0;
     private int max;
     private T[] list;
-    private T item;
-    private T first;
-    private T last;
 
-    public ArrayDeque () {
-        list = (T []) new Object[8];
+    public ArrayDeque() {
+        list = (T[]) new Object[8];
         max = 8;
-        first = list[0];
-        last = list[size];
     }
 
     public void addFirst(T item) {
@@ -22,7 +17,7 @@ public class ArrayDeque<T> {
         list[0] = item;
         size += 1;
     }
-    
+
     public void addLast(T item) {
         indexOverOrNot();
         list[size] = item;
@@ -33,17 +28,58 @@ public class ArrayDeque<T> {
         return (size == 0);
     }
 
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(list[i]);
+            System.out.print(' ');
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        T result = list[0];
+        System.arraycopy(list, 1, list, 0, size);
+        size -= 1;
+        indexOverOrNot();
+        return result;
+    }
+
+    public T removeLast() {
+        T result = list[size];
+        list[size] = null;
+        size -= 1;
+        indexOverOrNot();
+        return result;
+    }
+
+    public T get(int index) {
+        if (index < size && index > 0) return list[index];
+        return null;
+    }
+
     private void indexOverOrNot() {
         if (size == max) {
             increaseSize();
-        } else if ((double) size/max < RFACTOR) {
+        } else if ((double) size / max < R_FACTOR) {
             lowerSize();
         }
     }
 
     private void lowerSize() {
+        T[] newList = (T[]) new Object[(int) (max / 2)];
+        System.arraycopy(list, 0, newList, 0, size);
+        list = newList;
+        max = (int) (max / 2);
     }
 
     private void increaseSize() {
+        T[] newList = (T[]) new Object[(int) (max * (1 + R_FACTOR))];
+        System.arraycopy(list, 0, newList, 0, size);
+        list = newList;
+        max = (int) (max * (1 + R_FACTOR));
     }
 }

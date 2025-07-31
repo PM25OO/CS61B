2b8ec767b313c0714062f3f638657f7b1a008ea2
PM25OO <3210636476@qq.com>
2025-07-31 16:50:50 +0800

    Finish proj1

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -2,23 +2,21 @@ package deque;
 
 public interface Deque<T> {
 
-    public void addFirst(T item);
+    void addFirst(T item);
 
-    public void addLast(T item);
+    void addLast(T item);
 
-    public default boolean isEmpty() {
+    default boolean isEmpty() {
         return (size() == 0);
     }
 
-    ;
+    int size();
 
-    public int size();
+    void printDeque();
 
-    public void printDeque();
+    T removeFirst();
 
-    public T removeFirst();
+    T removeLast();
 
-    public T removeLast();
-
-    public T get(int index);
+    T get(int index);
 }

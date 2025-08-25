c19b508562b459f080c48b6e40114c4bb1f07e0b
PM25OO <3210636476@qq.com>
2025-08-25 22:43:18 +0800

    Finish BSTMap.java

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,112 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<k extends Comparable<k>, v> implements Map61B<k, v> {
+
+    private BSTNode root;
+
+    private class BSTNode {
+        private k key;
+        private v val;
+        private BSTNode left;
+        private BSTNode right;
+        private int size;
+
+        BSTNode(k k, v v, int size) {
+            key = k;
+            val = v;
+            this.size = size;
+        }
+
+    }
+
+    /**
+     * Removes all of the mappings from this map.
+     */
+    @Override
+    public void clear() {
+        root = null;
+    }
+
+    @Override
+    public boolean containsKey(k key) {
+        return containsKey(root, key);
+    }
+
+    private boolean containsKey(BSTNode node, k key) {
+        if (node == null) return false;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) return containsKey(node.left, key);
+        else if (cmp > 0) return containsKey(node.right, key);
+        else return true;
+    }
+
+    @Override
+    public v get(k key) {
+        return get(root, key);
+    }
+
+    private v get(BSTNode node, k key) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) return get(node.left, key);
+        else if (cmp > 0) return get(node.right, key);
+        else return node.val;
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(BSTNode node) {
+        if (node == null) return 0;
+        return node.size;
+    }
+
+    @Override
+    public void put(k key, v value) {
+        root = put(root, key, value);
+    }
+
+    private BSTNode put(BSTNode node, k key, v value) {
+        if (node == null) return new BSTNode(key, value, 1);
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) node.left = put(node.left, key, value);
+        else if (cmp > 0) node.right = put(node.right, key, value);
+        else node.val = value;
+        node.size = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    public void printInOrder() {
+
+    }
+
+    @Override
+    public Set<k> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public v remove(k key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public v remove(k key, v value) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<k> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}

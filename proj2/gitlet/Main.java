beb9ffbf67f2178cd56baa27b177b7ec2ce6a840
PM25OO <3210636476@qq.com>
2025-11-02 20:16:17 +0800

    fix format

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,22 +1,27 @@
 package gitlet;
 
-/** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author TODO
+/**
+ * Driver class for Gitlet, a subset of the Git version-control system.
+ *
+ * @author TODO
  */
 public class Main {
 
-    /** Usage: java gitlet.Main ARGS, where ARGS contains
-     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
+    /**
+     * Usage: java gitlet.Main ARGS, where ARGS contains
+     * <COMMAND> <OPERAND1> <OPERAND2> ...
      */
     public static void main(String[] args) {
-        if(args == null) {
+        if (args == null) {
             System.out.println("Please enter a command.");
             return;
         }
         String firstArg = args[0];
-        switch(firstArg) {
+        switch (firstArg) {
             case "init":
-                Repository.init();
+                if (args.length != 1) {
+                    System.out.println("Incorrect operands.");
+                } else Repository.init();
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command

45b01a3eb28e248279f4043c2d900e4c5f8de9c7
PM25OO <3210636476@qq.com>
2025-11-29 19:08:42 +0800

    complete AutograderBuddy

--- sp24-proj2b/src/main/AutograderBuddy.java
+++ sp24-proj2b/src/main/AutograderBuddy.java
@@ -9,6 +9,9 @@ public class AutograderBuddy {
             String wordFile, String countFile,
             String synsetFile, String hyponymFile) {
 
-        throw new RuntimeException("Please fill out AutograderBuddy.java!");
+        WordnetGraph graph = new WordnetGraph(synsetFile, hyponymFile);
+
+        return new HyponymsHandler(graph);
+
     }
 }
